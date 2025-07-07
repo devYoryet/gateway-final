001d1104f99bee88f6250ec1aab59c61cbe8a976
yoryet <y.danoun@pharmatender.cl>
2025-07-07 17:51:53 -0400

    feat :_ permitir todo

--- src/main/java/com/zosh/config/SecurityConfig.java
+++ src/main/java/com/zosh/config/SecurityConfig.java
@@ -2,17 +2,9 @@ package com.zosh.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfiguration;
@@ -25,31 +17,14 @@ import java.util.Collections;
 public class SecurityConfig {
 
         @Bean
-        public SecurityWebFilterChain springSecurityFilterChain(
-                        ServerHttpSecurity serverHttpSecurity) {
-                serverHttpSecurity.authorizeExchange(
-                                exchanges -> exchanges
-                                                .pathMatchers("/auth/**").permitAll()
-                                                .pathMatchers("/api/notifications/ws/**").permitAll()
-                                                .pathMatchers(
-                                                                "/api/salons/**",
-                                                                "/api/categories/**",
-                                                                "/api/notifications/**",
-                                                                "/api/bookings/**",
-                                                                "/api/payments/**",
-                                                                "/api/service-offering/**",
-                                                                "/api/users/**",
-                                                                "/api/reviews/**")
-                                                .hasAnyRole("CUSTOMER", "SALON_OWNER", "ADMIN")
-                                                .pathMatchers("/api/categories/salon-owner/**",
-                                                                "/api/notifications/salon-owner/**",
-                                                                "/api/service-offering/salon-owner/**")
-                                                .hasRole("SALON_OWNER"))
-                                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                                                .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(
-                                                                grantedAuthoritiesExtractor())));
-                serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+        public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+                serverHttpSecurity
+                                .authorizeExchange(exchanges -> exchanges
+                                                .anyExchange().permitAll() // 🔥 TEMPORAL: Permitir todo
+                                )
+                                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
                 return serverHttpSecurity.build();
         }
 
@@ -57,10 +32,7 @@ public class SecurityConfig {
                 CorsConfiguration configuration = new CorsConfiguration();
                 configuration.setAllowedOrigins(Arrays.asList(
                                 "http://localhost:3000",
-                                "https://salon-booking-three.vercel.app",
-                                "http://localhost:5000",
-
-                                "http://localhost:5170"));
+                                "https://salon-booking-three.vercel.app"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                 configuration.setAllowedHeaders(Collections.singletonList("*"));
                 configuration.setExposedHeaders(Collections.singletonList("Authorization"));
@@ -71,11 +43,4 @@ public class SecurityConfig {
                 source.registerCorsConfiguration("/**", configuration);
                 return source;
         }
-
-        private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
-                JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-                jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
-                return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
-        }
-
 }
